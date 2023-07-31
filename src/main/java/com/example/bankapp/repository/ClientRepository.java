e4a16bdbd35332a9eeac758cd6be1b6892df5cca
Yan <gorez9999@gmail.com>
2023-07-31 12:38:01 +0200

    ClientRepository

--- src/main/java/com/example/bankapp/repository/ClientRepository.java
+++ src/main/java/com/example/bankapp/repository/ClientRepository.java
@@ -14,6 +14,15 @@ public interface ClientRepository extends JpaRepository <Client, UUID> {
     @Query ("SELECT a from Client a where a.status = ?1")
     List<Client> getAllClientsWhereStatusIsActive(String status);
 
-    @Query ("SELECT a from Client a where a.balance = ?1")
+    @Query("SELECT c FROM Client c JOIN c.accountList a WHERE a.balance > ?1")
     List<Client> getAllClientsWhereBalanceMoreThan(BigDecimal balance);
+
+    @Query("SELECT c FROM Client c " +
+            "JOIN c.accountList a " +
+            "JOIN a.transactionDebit tDebit " +
+            "JOIN a.transactionCredit tCredit " +
+            "WHERE (SIZE(tDebit) + SIZE(tCredit)) > ?1")
+    List<Client> getAllClientsWhereTransactionMoreThan(int transactionCount);
+
+
 }
